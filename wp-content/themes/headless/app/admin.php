d9c3b81fa78b45be19fbd3c2d94272f19c0d15c8
Tom Macready-Bryan <tom@tommb.com.au>
2019-03-26 17:27:11 +1100

    Add post_parent = 0 action when adding a new image

--- wp-content/themes/headless/app/admin.php
+++ wp-content/themes/headless/app/admin.php
@@ -22,3 +22,13 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+add_action( 'add_attachment', function( $post_id ) { 
+  // set post parent to 0
+  wp_update_post(
+    array(
+      'ID' => $post_id, 
+      'post_parent' => 0
+    )
+  );
+});
